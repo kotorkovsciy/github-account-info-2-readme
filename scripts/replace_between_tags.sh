cc22d9531f979106c291151d1934b0f79191e026
kotorkovsciy <kotorkovsciy@gmail.com>
2025-01-04 03:33:42 +0300

    refactor(ci): simplify and modularize latest project update in README
    - Replaced direct curl request with GitHub CLI (gh api) for better integration.
    - Modularized tag replacement logic into a standalone script for reusability.
    - Improved readability and maintainability of the workflow.

---
+++ scripts/replace_between_tags.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+file="$1"
+new_content="$2"
+TAG="$3"
+
+sed -i "/<!--START_$TAG-->/, /<!--END_$TAG-->/ {
+    /<!--START_$TAG-->/! { 
+    /<!--END_$TAG-->/! s|.*|${{ env.latest_project }}| 
+  }
+}" "$file"
