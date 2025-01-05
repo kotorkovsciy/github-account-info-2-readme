f3cc41a2d2e42fc1732faafad4f5f672e9e2a538
kotorkovsciy <kotorkovsciy@gmail.com>
2025-01-05 08:54:30 +0300

    fix: escape special characters in new_content to prevent sed errors

--- scripts/replace_between_tags.sh
+++ scripts/replace_between_tags.sh
@@ -2,10 +2,8 @@ file="$1"
 new_content="$2"
 TAG="$3"
 
-new_content=$(printf '%s' "$new_content" | sed 's/[&/\]/\\&/g')
-
 sed -i "/<!--START_$TAG-->/, /<!--END_$TAG-->/ {
     /<!--START_$TAG-->/! { 
-    /<!--END_$TAG-->/! s|.*|$new_content| 
+    /<!--END_$TAG-->/! s#$#${new_content}# 
   }
 }" "$file"
